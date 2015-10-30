e7b849ed92a4954ac3ea5a819889247489d30ca5
Ryan Lee <ryan9206@hotmail.com>
2015-10-30 01:10:29 -0700

    Grabbing profile url to console
    I am still working on how to make that text into a variable.. seems
    like it’s a bit hard to find related documents

--- nav_controllers/MyProfilesViewControllers/MyProfilesViewController3.js
+++ nav_controllers/MyProfilesViewControllers/MyProfilesViewController3.js
@@ -6,16 +6,25 @@ exports.FBSDKLoginButton = require('../../node_modules/react-native-fbsdklogin/j
 exports.FBSDKLoginManager = require('../../node_modules/react-native-fbsdklogin/js-modules/FBSDKLoginManager.ios.js');
 
 var React = require('react-native');
+var FBSDKCore = require ('react-native-fbsdkcore');
+var FBSDKLogin = require('react-native-fbsdklogin');
 var TableView = require('react-native-tableview')
 var Section = TableView.Section;
 var Item = TableView.Item;
-var FBSDKLogin = require('react-native-fbsdklogin');
+
 var {
 	FBSDKLoginButton,
   	FBSDKLoginManager,
 } = FBSDKLogin;
 
 var {
+  FBSDKAccessToken,
+  FBSDKGraphRequest,
+  FBSDKGraphRequestManager,
+} = FBSDKCore;
+
+var {
+  Text,
   StyleSheet,
   Component,
   View
@@ -24,7 +33,13 @@ var {
 var styles = StyleSheet.create({
 });
 
-class MyProfilesViewController3 extends Component {
+var fetchURL = new FBSDKGraphRequest ((error, result) => {
+  console.log('FBSDKGraphRequest', error, result);
+  }, 'me?fields=link');
+FBSDKGraphRequestManager.batchRequests([fetchURL], function() {}, 60);
+
+
+class MyProfilesViewController3 extends Component { 
 	render() {
     return (
       <View>
@@ -37,28 +52,43 @@ class MyProfilesViewController3 extends Component {
                 alert('Login cancelled.');
               } else {
                 alert('Logged in.');
+                var token = new FBSDKAccessToken.getCurrentAccessToken(token => console.log (token, 'Type of Token is:' + typeof token));              
+                <Text style={styles.appendURL}>
+  Welcome to React Native!
+</Text>
               }
             }
           }}
-          onLogoutFinished={() => alert('Logged out.')}
+          onLogoutFinished={() => alert('Logged out.')} 
           readPermissions={[]}
-          publishPermissions={['publish_actions']}/>
+          publishPermissions={['publish_actions']}/>             
       </View>
     );
   }
 }
 
-// FBSDKLoginManager.setLoginBehavior(GlobalStore.getItem('behavior', 'native'));
-// FBSDKLoginManager.logInWithReadPermissions([], (error, result) => {
-//   if (error) {
-//     alert('Error logging in.');
-//   } else {
-//     if (result.isCanceled) {
-//       alert('Login cancelled.');
-//     } else {
-//       alert('Logged in.');
-//     }
-//   }
-// });
+
+
+var styles = StyleSheet.create({
+  appendURL: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+FBSDKLoginManager.setLoginBehavior('native');
+FBSDKLoginManager.logInWithReadPermissions([], (error, result) => {
+  if (error) {
+    alert('Error logging in.');
+  } else {
+    if (result.isCanceled) {
+      alert('Login cancelled.');
+    } else {
+      alert('Logged in.');
+    }
+  }
+});
+
 
 module.exports = MyProfilesViewController3;
