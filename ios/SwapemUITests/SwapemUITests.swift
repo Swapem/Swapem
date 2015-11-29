5ced8a3b8642ff9428f66cded31bf40a28b8969d
Lisa Wong <miss.lisa19@hotmail.com>
2015-11-28 23:48:53 -0800

    adding ui test

---
+++ ios/SwapemUITests/SwapemUITests.swift
@@ -0,0 +1,74 @@
+//
+//  SwapemUITests.swift
+//  SwapemUITests
+//
+//  Created by Lisa Wong on 2015-11-28.
+//  Copyright © 2015 Facebook. All rights reserved.
+//
+
+import XCTest
+
+class SwapemUITests: XCTestCase {
+        
+  override func setUp() {
+      super.setUp()
+      
+      // Put setup code here. This method is called before the invocation of each test method in the class.
+      
+      // In UI tests it is usually best to stop immediately when a failure occurs.
+      continueAfterFailure = true
+      // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+      XCUIApplication().launch()
+
+      // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+  }
+
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+
+  func testExample() {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    
+    
+    let app = XCUIApplication()
+    app.otherElements["     school  "].otherElements["    school  "].tap()
+    
+    let facebookComElement = app.otherElements["                        facebook.com/   "]
+    let element = facebookComElement.childrenMatchingType(.Other).matchingIdentifier("    ").elementBoundByIndex(0)
+    element.doubleTap()
+    
+    let pasteMenuItem = app.menuItems["Paste"]
+    pasteMenuItem.tap()
+    facebookComElement.childrenMatchingType(.Other).matchingIdentifier("    ").elementBoundByIndex(1).tap()
+    
+    let lisaWongFacebookComElement = app.otherElements["         Lisa Wong                facebook.com/   "]
+    lisaWongFacebookComElement.childrenMatchingType(.Other).matchingIdentifier("    ").elementBoundByIndex(0).tap()
+    pasteMenuItem.tap()
+    lisaWongFacebookComElement.childrenMatchingType(.Other).matchingIdentifier("    ").elementBoundByIndex(1).tap()
+    app.otherElements["         Lisa Wong      Lisa Wong           facebook.com/   "].otherElements["    "].tap()
+    pasteMenuItem.tap()
+    app.navigationBars["school"].buttons["Save"].tap()
+    app.alerts["Alert"].collectionViews.buttons["OK"].tap()
+    
+  }
+  
+  func testProfile(){
+    
+    
+    let app = XCUIApplication()
+    app.navigationBars["My Profiles"].buttons["Add"].tap()
+    
+    let collectionViewsQuery = app.alerts["Enter profile name"].collectionViews
+    collectionViewsQuery.buttons["Save"].tap()
+    collectionViewsQuery.buttons["Cancel"].tap()
+
+
+    
+    
+    
+  }
+}
